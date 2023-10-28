f70abf70ed0bef7fd8513693907abef4188e6d1a
Melarn <melarn06@gmail.com>
2023-10-28 11:29:41 +0300

    Cleaned up sm

--- src/main/java/me/melarn/fasteronblock/FasterBlockEvent.java
+++ src/main/java/me/melarn/fasteronblock/FasterBlockEvent.java
@@ -1,7 +1,6 @@
 package me.melarn.fasteronblock;
 
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,11 +11,11 @@ import org.bukkit.potion.PotionEffectType;
 public class FasterBlockEvent implements Listener {
 
     @EventHandler
-    public void onPlayerWalk(PlayerMoveEvent event){
+    public void onPlayerWalk(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        if (player.getLocation().getBlock().getType().equals(Material.DIRT_PATH)){
-//            player.sendMessage("you stood on block");
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 25, (int) 0.3f).withParticles(false).withIcon(false));
+        if (player.getLocation().getBlock().getType().equals(Material.DIRT_PATH)) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 25,
+                    (int) 0.3f).withParticles(false).withIcon(false));
         }
     }
 }
